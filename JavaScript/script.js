c1973aa6aa39d613e27d36a0911ed9d939304923
Tanya Garg <tgarg1012@gmail.com>
2026-07-06 16:33:25 +0530

    Practiced JavaScript basics

---
+++ JavaScript/script.js
@@ -0,0 +1,51 @@
+console.log("JS Practice");
+
+let name = "Tanya";
+let age = 20;
+const college = "ABESIT";
+var city = "Delhi";
+
+console.log(name);
+console.log(age);
+console.log(college);
+console.log(city);
+
+console.log(typeof name);
+console.log(typeof age);
+
+let a = 20;
+let b = 5;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+
+console.log(10 + 5 * 2);
+console.log((10 + 5) * 2);
+
+console.log(0 / 0);
+console.log("abc" * 2);
+
+let marks = 80;
+
+marks += 10;
+console.log(marks);
+
+marks -= 5;
+console.log(marks);
+
+let count = 1;
+
+count++;
+console.log(count);
+
+++count;
+console.log(count);
+
+count--;
+console.log(count);
+
+console.log(5 == "5");
+console.log(5 === "5");

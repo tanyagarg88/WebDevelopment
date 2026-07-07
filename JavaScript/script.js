7f594287b2cec9e309f191128187d50a3469e20d
Tanya Garg <tgarg1012@gmail.com>
2026-07-07 17:17:03 +0530

    conditional statements

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -78,3 +78,41 @@ console.log(msg[1]);
 console.log(msg[msg.length - 1]);
 
 console.log("Practice Complete");
+let x= 25;
+let y=85;
+console.log("sum is : ",x+y);
+let s=2;
+let g=10;
+console.log(s+g);
+console.log(s-g);
+console.log(s*g);
+console.log(s/g);
+console.log(s**g);
+console.log(s%g);
+console.log(s++);
+console.log(++s);
+console.log(s+=g);
+
+age=18;
+console.log(age<18);
+ 
+console.log('p'<'P');
+console.log('A'>'b');
+
+console.log("before my if statement");
+let newAge=23;
+if (newAge>=18){
+    console.log("you can vote");
+}
+console.log("after my if statement");
+
+let color="green";
+if(color="red"){
+    console.log("stop");
+}
+if (color="yellow"){
+    console.log("ready");
+}
+if(color="green"){
+    console.log("go");
+}

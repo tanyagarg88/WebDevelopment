33790dea0087af59a4521e8c7f13e82bb96a5d9c
Tanya Garg <tgarg1012@gmail.com>
2026-07-13 20:02:10 +0530

    added the app.js content

---
+++ starter js/app.js
@@ -0,0 +1,12 @@
+let smallImages = document.getElementsByClassName("oldImag");
+
+for(let i=0;i<smallImages.length;i++){
+    console.dir(smallImages[i]);
+}
+
+console.dir(document.querySelector("h1"));
+
+console.dir(document.querySelector("#description"));
+console.dir(document.querySelector(".oldImg"));
+console.dir(document.querySelector("div a"));
+console.dir(document.querySelectorAll("h1"));

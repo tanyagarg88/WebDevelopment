1f0423ccb8677655d799376ed467982d062a94cd
Tanya Garg <tgarg1012@gmail.com>
2026-07-16 00:57:40 +0530

    Added random color generator using DOM events

--- DOM/scriptt.js
+++ DOM/scriptt.js
@@ -1,12 +1,50 @@
-let btn = document.querySelector("button");
-console.dir(btn);
+//let btn = document.querySelector("button");
+//console.dir(btn);
 
-function sayhello() {
-    alert("Aware!!");
-}
+//function sayhello() {
+ //   alert("Aware!!");
+//}
 
-btn.onclick = sayhello;
-btn.onmouseenter = function(){
-    console.log("you enter a button");
+//btn.onclick = sayhello;
+//btn.onmouseenter = function(){
+ //   console.log("you enter a button");
+
+//}
+
+let actionButton = document.querySelector("button");
+
+actionButton.addEventListener("click", function () {
+
+    let h3 = document.querySelector("h3");
+
+    let randomColor = getRandomColor();
+
+    h3.innerText = randomColor;
+
+    let div = document.querySelector("div");
+
+    div.style.backgroundColor = randomColor;
+
+    console.log("Color Updated");
+
+});
+
+//actionButton.addEventListener("mouseenter", function () {
+   // console.log("Mouse Enter");
+//});
+
+//actionButton.addEventListener("mouseleave", function () {
+  //  console.log("Mouse Leave");
+//});
+
+function getRandomColor() {
+
+    let red = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);
+    let blue = Math.floor(Math.random() * 256);
+
+    let color = `rgb(${red}, ${green}, ${blue})`;
+
+    return color;
 
 }

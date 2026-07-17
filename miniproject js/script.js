62bb7919909e44e400c3671bec99deccf8372fc0
Tanya Garg <tgarg1012@gmail.com>
2026-07-17 19:45:17 +0530

    learn visualizing call stack concept

--- miniproject js/script.js
+++ miniproject js/script.js
@@ -57,3 +57,26 @@ ol.addEventListener("click", function (event) {
     }
 
 });
+
+function hello(){
+    console.log("inside hello function");
+    console.log("hello");
+}
+function demo(){
+    console.log("calling hello function");
+    hello();
+}
+console.log("calling demo function");
+demo();
+console.log("DOne!!,bye");
+
+function one(){
+    return 1;
+}
+function two(){
+    return one()+one();
+}
+function three(){
+    let ans = two()+one();
+    console.log(ans);
+}

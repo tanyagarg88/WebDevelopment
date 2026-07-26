ebb21d9828aa21df49fc06798f6823099fb7f799
tanya <tgarg1012@gmail.com>
2026-07-26 18:15:58 +0530

    update the backend folder with math.js file and learn require

---
+++ Backend/math.js
@@ -0,0 +1,14 @@
+const sum =(a,b) =>a+b;
+const mul =(a,b) =>a*b;
+const div =(a,b) =>a/b;
+const sub =(a,b) =>a-b;
+
+let obj={
+    sum:sum,
+    mul:mul,
+    div:div,
+    sub:sub,
+};
+
+
+module.exports = obj;
--- Backend/script.js
+++ Backend/script.js
@@ -3,3 +3,12 @@ for (i=0;i<=5;i++){
     console.log("hello",+i);
 }
 console.log("bye");
+
+
+let argv = process.argv;
+for(let j=2;j<argv.length;j++){
+    console.log("hello ",argv[j]);
+}
+
+const math=require("./math");
+console.log(math);

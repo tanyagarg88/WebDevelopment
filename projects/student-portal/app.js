32126010fa1a0043b7f1a080baf9a7c1707acbc7
Tanya Garg <tgarg1012@gmail.com>
2026-07-28 10:10:48 +0530

    Build Student Portal Express server with EJS template engine (Jul 28)

---
+++ projects/student-portal/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+let students = [
+  { id: '1', name: 'Aarav Sharma', course: 'Computer Science', grade: 'A' },
+  { id: '2', name: 'Ananya Patel', course: 'Data Science', grade: 'A+' },
+  { id: '3', name: 'Rohan Verma', course: 'Cybersecurity', grade: 'B+' }
+];
+
+app.get('/students', (req, res) => {
+  res.render('index', { students });
+});
+
+module.exports = app;
+
+
+<!-- Updated: 2026-07-28T10:10:48+05:30 [Commit #412] -->

af3a0e3c5128c4775474aa5db33a3b3873d34cd9
tgarg1012@gmail.com <tgarg1012@gmail.com>
2026-08-05 01:50:53 +0530

    Add Express.js middleware and backend practice

---
+++ Backend/expressDir/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+app.use((req,res,next) =>{
+    console.log("Request URL:", req.url);
+    next();
+});

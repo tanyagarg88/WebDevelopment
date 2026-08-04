d9149d0bc03616036b814cbb41e33fe73759f0f7
tgarg1012@gmail.com <tgarg1012@gmail.com>
2026-08-05 00:46:54 +0530

    added the figletdir

---
+++ Backend/FigletDir/index.js
@@ -0,0 +1,10 @@
+const figlet = require('figlet');
+
+figlet("TANYA GARG!!", function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data);
+});

a381dadba33041ec410604968ea9483cdc6cd3b5
tgarg1012@gmail.com <tgarg1012@gmail.com>
2026-08-06 00:26:31 +0530

    adding views folder

--- Backend/EJSdir/index.js
+++ Backend/EJSdir/index.js
@@ -9,6 +9,16 @@ app.get("/",(req,res)=>{
     res.render("home.ejs");
 });
 
+app.get("/rolldice",(req,res)=>{
+    let diceVal =Math.floor(Math.random() * 6) + 1;
+    res.render("rolldice.ejs", { diceVal });
+});
+
+app.get("/ig/:username", (req, res) => {
+    let { username } = req.params;
+    res.render("instagram", { username });
+});
+
 app.listen(port,()=>{
     console.log('listening on port ${port}');
 });

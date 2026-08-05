5b802667495fcb512ec1b61450eea6cefa1701a1
tgarg1012@gmail.com <tgarg1012@gmail.com>
2026-08-05 16:48:26 +0530

    update the backend ejs and view folders

--- Backend/EJSdir/index.js
+++ Backend/EJSdir/index.js
@@ -1,7 +1,13 @@
-const express =require("express");
+const express = require("express");
 const app = express();
 
 const port =8080;
+
+app.set("view engine","ejs");
+
+app.get("/",(req,res)=>{
+    res.send("this is home");
+});
 
 app.listen(port,()=>{
     console.log('lsitening on port ${port}');

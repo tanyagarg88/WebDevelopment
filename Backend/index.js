52102fee3a159e9d093b7ba5dc67642ab383b3dc
tgarg1012@gmail.com <tgarg1012@gmail.com>
2026-08-07 01:13:14 +0530

    modifies backend and frontend

--- Backend/index.js
+++ Backend/index.js
@@ -1,15 +1,25 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
 const app = express();
-const port = 3000;
+const port = 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
 
 app.get("/register", (req, res) => {
-    res.send("Standard Get Request");
+    let { user, password } = req.query;
+    res.send(`Standard Get Request, ${user}!`);
 });
 
 app.post("/register", (req, res) => {
-    res.send("Standard Post Request");
+    let { user, password } = req.body;
+    res.send(`Standard Post Request, ${user}!`);
 });
 
-app.listen(port,()=>{
-    console.log(`listening on port ${port}`);
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
 });

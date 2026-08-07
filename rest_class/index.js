e091bfe0c9f3c3dcf97a5fbeb29012d8f90ecd88
tgarg1012@gmail.com <tgarg1012@gmail.com>
2026-08-08 01:42:19 +0530

    Adding new rest_class

---
+++ rest_class/index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const path=require("path");
+
+app.use(express.urlencoded({ extended: true }));
+
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.set(express.static(path.join(__dirname, "public")));
+
+let posts =[
+    {
+    username:"tanyagarg",
+    content:"this is my first post"
+    },
+    {
+    username:"Btech",
+    content:"this is my second post"
+    },
+    {
+    username:"CSE",
+    content:"this is my third post"
+    },
+    {
+    username:"honesty",
+    content:"this is my forth post"
+    },
+];
+
+app.get("/", (req, res) => {
+    res.send("server working well");
+});
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});

bcfbd02369aae3a0ec5d0f39c3a14de3811059f9
tgarg1012@gmail.com <tgarg1012@gmail.com>
2026-08-08 16:20:56 +0530

    added new.ejs in rest_class

--- rest_class/index.js
+++ rest_class/index.js
@@ -33,6 +33,17 @@ let posts =[
 app.get("/posts", (req, res) => {
     res.render("index.ejs", { posts: posts });
 });
+app.get("/posts/new", (req, res) => {
+    res.render("new.ejs");
+});
+app.post("/posts", (req, res) => {
+    let {username,content}=req.body;
+    posts.push({username,content});
+
+    res.redirect("/posts");
+});
+
+app.get
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });

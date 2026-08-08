5b3a39cb5353da9ddd47193cab7a83be5bc23481
tgarg1012@gmail.com <tgarg1012@gmail.com>
2026-08-09 00:17:49 +0530

    modified the redt_class and apply CRUD operations

--- rest_class/index.js
+++ rest_class/index.js
@@ -2,29 +2,37 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const path=require("path");
+const {v4: uuidv4} = require("uuid");
+const methodOverride=require("method-override");
 
+
+app.use(methodOverride("_method"));
 app.use(express.urlencoded({ extended: true }));
 
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("public"));
 
 let posts =[
     {
+    id:uuidv4(),
     username:"tanyagarg",
     content:"this is my first post"
     },
     {
+    id:uuidv4(),
     username:"Btech",
     content:"this is my second post"
     },
     {
+    id:uuidv4(),
     username:"CSE",
     content:"this is my third post"
     },
     {
+    id:uuidv4(),
     username:"honesty",
     content:"this is my forth post"
     },
@@ -38,12 +46,37 @@ app.get("/posts/new", (req, res) => {
 });
 app.post("/posts", (req, res) => {
     let {username,content}=req.body;
-    posts.push({username,content});
+    let id=uuidv4();
+    posts.push({id,username,content});
 
     res.redirect("/posts");
 });
+app.get("/posts/:id", (req, res) => {
+    let{id}=req.params;
+    let post=posts.find((p)=>id ===p.id);
+    res.render("show.ejs", { post: post });
+});
 
-app.get
+app.patch("/posts/:id", (req, res) => {
+    let {id} = req.params;
+    let newContent = req.body.content;
+    let post=posts.find((p)=>id ===p.id);
+    post.content=newContent;
+    console.log(posts);
+    res.redirect("/posts");
+});
+app.get("/posts/:id/edit", (req, res) => {
+    let {id} = req.params;
+    let post=posts.find((p)=>id ===p.id);
+    res.render("edit.ejs", { post: post });
+});
+
+app.delete("/posts/:id", (req, res) => {
+    let {id} = req.params;
+    posts=posts.filter((p)=>id !==p.id);
+    res.redirect("/posts");
+});
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });

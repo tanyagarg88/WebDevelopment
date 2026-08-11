c5d0fa4cf559e66c2cb9440fa09bd67451483067
tgarg1012@gmail.com <tgarg1012@gmail.com>
2026-08-12 01:24:17 +0530

    mysql2

--- Backend/SQLclass/index.js
+++ Backend/SQLclass/index.js
@@ -1,14 +1,27 @@
-const { faker } = require('@faker-js/faker');;
+const { faker } = require('@faker-js/faker');
+const mysql = require('mysql2');
 
-let createRandomUser = () => {
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'tanya',
+  database: 'delta_app',
+});
+
+connection.query("SHOW TABLES", (err, results) => {
+  if (err) {
+    console.error('Error executing query:', err);
+    return;
+  }
+});
+
+let getRandomUser = () => {
   return {
     userId: faker.string.uuid(),
     username: faker.internet.username(),
     email: faker.internet.email(),
-    avatar: faker.image.avatar(),
     password: faker.internet.password(),
-    birthdate: faker.date.birthdate(),
-    registeredAt: faker.date.past(),
   };
-  console.log(createRandomUser());
 }
+
+  console.log(getRandomUser());
